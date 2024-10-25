ad5f01ab88a22470da9ff383ea88a7506da46cf4
sandy010503 <sundeepsandy2003@gmail.com>
2024-10-25 12:36:46 +0530

    New AddOns

--- angular/src/components/logins/clientlogin/clientlogin.component.ts
+++ angular/src/components/logins/clientlogin/clientlogin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-clientlogin',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,RouterOutlet],
   templateUrl: './clientlogin.component.html',
   styleUrl: './clientlogin.component.css'
 })
--- angular/src/services/clientservice.service.ts
+++ angular/src/services/clientservice.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,11 @@ export class ClientserviceService {
  
   _https:HttpClient;
 
-  constructor(_httpsRef:HttpClient)
+  constructor(_httpsRef:HttpClient, private router:Router)
   {
     this._https=_httpsRef;
   }
+  
 
   getConsultantDetails()
   {
@@ -37,5 +40,43 @@ export class ClientserviceService {
       console.log('added',response);
       alert("added");
     })
+  }
+
+  //Login
+
+  redirecttologinpage(){
+    this.router.navigate(['/client']);
+  }
+  customerId:any;
+  loginDetails(loginInfo:any)
+  {
+
+    console.log(loginInfo.value);
+
+    this._https.get("https://consultantwebapiazure.azurewebsites.net/api/Users/userName?name=" + loginInfo.value.userName)
+    .subscribe((data=>{
+      this.customerId=data;
+      console.log(this.customerId)
+    }))
+    
+    
+    
+    // console.log(loginInfo.value.userRole);
+   return this._https.post("https://consultantwebapiazure.azurewebsites.net/api/Users/Login",loginInfo.value,this.httpOptions).pipe(
+      tap(response => {
+        // Handle success response
+        console.log("Success", response);
+        // alert("Login Successful");
+        this.redirecttologinpage();
+        //this.router.navigate(['/home']);
+        // this.router.navigate(['/your-target-component']); // Replace with your target route
+      }),
+      catchError(error => {
+        // Handle error response
+        console.error("Error", error);
+        alert(error.error || "Login failed. Please try again.");
+        return throwError(() => new Error('test')); // Rethrow the error for further handling if needed
+      })
+    ).subscribe(); // Subscribe at the end to trigger the request
   }
 }
